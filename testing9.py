68fb65b09ddfe4bcf9b754860a506448ba15dc4b
Rupinder <rupindermonga@gmail.com>
2020-05-24 20:27:54 +0530

    recurssion depth failure

--- testing9.py
+++ testing9.py
@@ -36,32 +36,6 @@ def add(data, p):
         add(data[key], key)
     return count
 
-def parentList(category):
-    # category = test_text
-    node_list = node_id[category]
-    parent_node_list = []
-    parent_position = 0
-    for eachValue in parent[category]:  
-        parent_position += 1
-        if eachValue == top:
-            parent_node_list.append("Zero")
-        else:
-            my_list = node_id[eachValue]
-            if len(my_list) > 1:
-                eachValue_index = parent_position - 1
-                my_number =  node_list[eachValue_index]
-                new_parent_id = min([ i for i in my_list if i < my_number], key=lambda x:abs(x-my_number))
-                parent_node_list.append(new_parent_id)
-            else:
-                parent_node_list.append(node_id[eachValue][0])
-    return parent_node_list
-
-def parentIList(category):
-    parent_column.append(parentList(eachCategory))
-    parent_final_list.append(parent[eachCategory])
-    node_final_list.append(node_id[eachCategory])
-    return parent_final_list, node_final_list, parent_column
-    
 pathList = []
 global_list = []
 path_keys = {}
@@ -86,37 +60,65 @@ category = 'Backpacks'
 creatingPathlist(category)
 print(global_list)
 
+
+# def parentList(category):
+#     # category = test_text
+#     node_list = node_id[category]
+#     parent_node_list = []
+#     parent_position = 0
+#     for eachValue in parent[category]:  
+#         parent_position += 1
+#         if eachValue == top:
+#             parent_node_list.append("Zero")
+#         else:
+#             my_list = node_id[eachValue]
+#             if len(my_list) > 1:
+#                 eachValue_index = parent_position - 1
+#                 my_number =  node_list[eachValue_index]
+#                 new_parent_id = min([ i for i in my_list if i < my_number], key=lambda x:abs(x-my_number))
+#                 parent_node_list.append(new_parent_id)
+#             else:
+#                 parent_node_list.append(node_id[eachValue][0])
+#     return parent_node_list
+
+# def parentIList(category):
+#     parent_column.append(parentList(eachCategory))
+#     parent_final_list.append(parent[eachCategory])
+#     node_final_list.append(node_id[eachCategory])
+#     return parent_final_list, node_final_list, parent_column
+    
+
 # print(path_keys[category])
 
 
-for eachFile in json_files:
-    try:
-        with open(os.path.join(path_to_json, eachFile)) as f:
-            new_file = json.load(f)
-    except:
-        pass
-    finally:
-        parent_column = []
-        parent_final_list =[]
-        node_final_list = []
-        top = eachFile.replace(".json","")
-        categories = []
-        parent = {}
-        count = 0
-        node_id = {}
-        categories.append(top)
-        adding_data = add(new_file, top)
-        del categories[0]
-        for eachCategory in categories:
-            parentIList(eachCategory)
-            # print(parent[eachCategory])
-            parent_list = parent[eachCategory]
-        final_dataFrame = pd.DataFrame({
-                                    "Category": categories,
-                                    "node_id": node_final_list, 
-                                    "parent_list": parent_final_list,
-                                    "parent_id": parent_column})
-        final_dataFrame.to_csv(os.path.join(save_path, top +".csv"))
+# for eachFile in json_files:
+#     try:
+#         with open(os.path.join(path_to_json, eachFile)) as f:
+#             new_file = json.load(f)
+#     except:
+#         pass
+#     finally:
+#         parent_column = []
+#         parent_final_list =[]
+#         node_final_list = []
+#         top = eachFile.replace(".json","")
+#         categories = []
+#         parent = {}
+#         count = 0
+#         node_id = {}
+#         categories.append(top)
+#         adding_data = add(new_file, top)
+#         del categories[0]
+#         for eachCategory in categories:
+#             parentIList(eachCategory)
+#             # print(parent[eachCategory])
+#             parent_list = parent[eachCategory]
+#         final_dataFrame = pd.DataFrame({
+#                                     "Category": categories,
+#                                     "node_id": node_final_list, 
+#                                     "parent_list": parent_final_list,
+#                                     "parent_id": parent_column})
+#         final_dataFrame.to_csv(os.path.join(save_path, top +".csv"))
 
 
 
